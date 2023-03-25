ae8ec2c1677018a2192a2fa04aa6d8c2d4bee815
boldizsarzoltan <boldizsarzoltanprograming@gmail.com>
2023-03-25 19:52:42 +0200

    Added python script

--- routes/upload.js
+++ routes/upload.js
@@ -1,7 +1,7 @@
 var express = require('express');
 const multer = require("multer");
 var router = express.Router();
-
+const { spawn } = require("child_process");
 router.post('/', function (req, res, next) {
   // req.file is the `avatar` file
   // req.body will hold the text fields, if there were any
@@ -21,7 +21,19 @@ var upload = multer({ storage: storage });
 // Export routes
 
 router.post('/fileupload', upload.single('picture'), (req, res, next) => {
-    res.status(200).json({message: `File uploaded successfully on '${process.cwd()}/uploads/${req.file.filename}`});
+    var service = spawn("python3", [
+        `${process.cwd()}` + "/test.py",
+        `${process.cwd()}/uploads/`+`${req.file.filename}`
+    ]);
+    let dataToSend;
+    service.stdout.on("data", function (data) {
+        dataToSend = data.toString();
+    });
+
+    service.on("close", (code) => {
+        res.status(200).json({message: dataToSend});
+    });
+    // res.status(200).json({message: `File uploaded successfully on '${process.cwd()}/uploads/${req.file.filename}'`});
 });
 
 module.exports = router;
---
+++ test.py
@@ -0,0 +1,4 @@
+import json
+import sys
+
+print(str.strip(sys.argv[1]))
