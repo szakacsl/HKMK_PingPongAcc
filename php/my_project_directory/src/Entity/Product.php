1e2bf53fb56fa6dce98c948db803530c6daf60e1
boldizsarzoltan <boldizsarzoltanprograming@gmail.com>
2023-03-25 11:36:54 +0200

    Added backened

---
+++ php/my_project_directory/src/Entity/Product.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\ManyToOne(inversedBy: 'products')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?ProductCollection $collection = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCollection(): ?ProductCollection
+    {
+        return $this->collection;
+    }
+
+    public function setCollection(?ProductCollection $collection): self
+    {
+        $this->collection = $collection;
+
+        return $this;
+    }
+}
