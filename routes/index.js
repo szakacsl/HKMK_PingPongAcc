696f2bfe14fc4efc65bdc0ed3a32da7d7ecbd0ae
David Hurubas <hurubas.david66@gmail.com>
2023-03-25 14:58:16 +0200

    basic script to execute a python file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const { spawn } = require("child_process");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  const service = spawn("python", ["..\\python_service\\dummy.py"]);
+
+  let dataToSend;
+
+  service.stdout.on("data", function (data) {
+    console.log("Pipe data from python script ...");
+    dataToSend = data.toString();
+
+    console.log(dataToSend);
+  });
+
+  service.on("close", (code) => {
+    console.log(`child process close all stdio with code ${code}`);
+    // send data to browser
+    // res.send(dataToSend);
+  });
+
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
