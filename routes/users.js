fb2cdc6090b96e64d4fe784e2407d3bf1ea53a01
boldizsarzoltan <boldizsarzoltanprograming@gmail.com>
2023-03-26 06:00:24 +0300

    Added user registration

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var db = require("../database/setup");
+const bcrypt = require("bcryptjs");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/register', async function (req, res, next) {
+  try {
+    const user = await db.user.create({
+      username: req.body.username,
+      password: bcrypt.hashSync(req.body.password, 8),
+    });
+    res.status(200).send('success');
+  }
+  catch (e) {
+    console.log(e);
+    res.status(500).send('fail');
+  }
 });
 
 module.exports = router;
